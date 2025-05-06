9c42c0a54bfccba60fc963c45776889b375f6243
Vladimir Sklenar <galaxia.andromeda234@gmail.com>
2025-05-06 23:29:00 +0200

    Fix Merge2

--- go.mod
+++ go.mod
@@ -10,4 +10,9 @@ require (
 	golang.org/x/crypto v0.36.0
 )
 
-require github.com/gorilla/securecookie v1.1.2 // indirect
+require (
+	github.com/coreos/go-log v0.0.0-20180308165134-b22fd89e1882 // indirect
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+)
--- handlers_country.go
+++ handlers_country.go
@@ -12,11 +12,11 @@ const (
 	cspLogin    = "12345678" // TODO: change before the game
 	cspPassword = "12345678" // TODO: change before the game
 	cspName     = "Joe Amík" // display in frontend
-	cspFinalURL = "/moje-csp"
+	cspFinalURL = "/moje-CSP"
 )
 
 func cspRouter() *chi.Mux {
-	r := newGameRouter("csp")
+	r := newGameRouter("CSP")
 	r.Get("/", auth(cspIndexGet))
 	r.Post("/", cspIndexPost)
 	r.Get(cspFinalURL, auth(cspIntranetGet))
@@ -34,7 +34,7 @@ func cspIndexGet(w http.ResponseWriter, r *http.Request) {
 
 	data := cspIndexData{
 		GeneralData: getGeneralData("CSP – Country Sdružení Pára", w, r),
-		Completed:   team.Csp.Completed,
+		Completed:   team.CSP.Completed,
 		Name:        cspName,
 	}
 	executeTemplate(w, "cspIndex", data)
@@ -51,30 +51,30 @@ func cspIndexPost(w http.ResponseWriter, r *http.Request) {
 	defer server.state.Unlock()
 
 	team := server.state.GetTeam(getUser(r))
-	if team != nil && (team.Csp.Completed) {
+	if team != nil && (team.CSP.Completed) {
 		http.Redirect(w, r, cspFinalURL, http.StatusSeeOther)
 		return
 	}
 
-	team.Csp.Tries++
+	team.CSP.Tries++
 
 	login := r.PostFormValue("login")
 	password := r.PostFormValue("password")
-	log.Infof("[Csp - %s] Trying login '%s' and password '%s'", team.Login, login, password)
+	log.Infof("[CSP - %s] Trying login '%s' and password '%s'", team.Login, login, password)
 
 	// Perform case-sensitive password comparison
 	if login == cspLogin && password == cspPassword {
-		log.Infof("[Csp - %s] Completed", team.Login)
+		log.Infof("[CSP - %s] Completed", team.Login)
 		// Everything completed
-		team.Csp.Completed = true
-		team.Csp.CompletedTime = time.Now()
+		team.CSP.Completed = true
+		team.CSP.CompletedTime = time.Now()
 		// Save state before redirecting (optional but safer)
 		server.state.Save()
 		setFlashMessage(w, r, messageOk, "Přihlášení bylo úspěšné, vítejte v systému Moje CSP")
 		http.Redirect(w, r, cspFinalURL, http.StatusSeeOther)
 	} else {
 		// Add more detailed logging for failed attempts
-		log.Warningf("[Csp - %s] Failed login attempt. Provided Login: '%s', Provided Password: '%s'. Expected Login: '%s', Expected Password: '%s'", team.Login, login, password, cspLogin, cspPassword)
+		log.Warningf("[CSP - %s] Failed login attempt. Provided Login: '%s', Provided Password: '%s'. Expected Login: '%s', Expected Password: '%s'", team.Login, login, password, cspLogin, cspPassword)
 		setFlashMessage(w, r, messageError, "Nesprávné číslo průkazu nebo heslo")
 		// Explicit redirect on failure (already present)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -83,14 +83,14 @@ func cspIndexPost(w http.ResponseWriter, r *http.Request) {
 
 func cspIntranetGet(w http.ResponseWriter, r *http.Request) {
 	team := server.state.GetTeam(getUser(r))
-	if team == nil || !team.Csp.Completed {
+	if team == nil || !team.CSP.Completed {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
 	data := cspIndexData{
 		GeneralData: getGeneralData("CSP – ", w, r),
-		Completed:   team.Csp.Completed,
+		Completed:   team.CSP.Completed,
 		Name:        cspName,
 	}
 
