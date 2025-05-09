57f54b25efb0de95b1a055a78f9767594591f4e8
Jan Kotovsky <kotovsky.jan@gmail.com>
2025-05-09 11:38:38 +0200

    klasicka updated

--- handlers_klasicka.go
+++ handlers_klasicka.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/coreos/go-log/log"
 	"github.com/go-chi/chi/v5"
 )
 
 func klasickaRouter() *chi.Mux {
 	r := newGameRouter("klasicka")
-	r.Get("/", klasickaIndexGet)
+	r.Get("/", auth(klasickaIndexGet))
+	r.Post("/", auth(klasickaIndexPost))
 	return r
 }
 
@@ -17,10 +20,50 @@ type klasickaIndexData struct {
 }
 
 func klasickaIndexGet(w http.ResponseWriter, r *http.Request) {
-	// team := server.state.GetTeam(getUser(r)) // Uncomment if team data is needed
+	team := server.state.GetTeam(getUser(r)) // Uncomment if team data is needed
 
-	data := klasickaIndexData{
-		GeneralData: getGeneralData("Hroší opera", w, r),
+	if team != nil && team.Klasicka.Completed {
+		data := klasickaIndexData{
+			GeneralData: getGeneralData("Hroší opera - Hacked", w, r),
+		}
+		executeTemplate(w, "klasickaHacked", data)
+	} else {
+		data := klasickaIndexData{
+			GeneralData: getGeneralData("Hroší opera", w, r),
+		}
+		executeTemplate(w, "klasickaIndex", data)
 	}
-	executeTemplate(w, "klasickaIndex", data)
+}
+
+
+func klasickaIndexPost(w http.ResponseWriter, r *http.Request) {
+	defer http.Redirect(w, r, "/", http.StatusSeeOther)
+
+	if err := r.ParseForm(); err != nil {
+		setFlashMessage(w, r, messageError, "Cannot parse form")
+		return
+	}
+
+	server.state.Lock()
+	defer server.state.Unlock()
+
+	team := server.state.GetTeam(getUser(r))
+	if team != nil && (team.Klasicka.Completed) {
+		return
+	}
+
+	
+	cislo := r.PostFormValue("cislo")
+	heslo := r.PostFormValue("heslo")
+	log.Infof("[Klasicka - %s] Trying login: '%s' and pswd: %d", team.Login, cislo, heslo)
+	if cislo == "78136" && heslo == "amadeus69" {
+		log.Infof("[Klasicka - %s] Login passed!", team.Login)
+		team.Klasicka.Completed = true
+		team.Klasicka.CompletedTime = time.Now()
+	}
+	// slog := slog.With("team", team.Login)
+
+	team.Klasicka.Tries++
+	team.Klasicka.LastTry = time.Now()
+	defer server.state.Save()
 }
